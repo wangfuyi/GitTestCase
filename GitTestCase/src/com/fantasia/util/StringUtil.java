14630a7c060c4b0562dd7e0a296fa9642d63cffd
wangfuyi <moyunyu5@gmail.com>
2013-08-12 00:32:22 +0800

    test

--- GitTestCase/src/com/fantasia/util/StringUtil.java
+++ GitTestCase/src/com/fantasia/util/StringUtil.java
@@ -2,7 +2,7 @@ package com.fantasia.util;
 
 public class StringUtil {
 	public static void main(String[] args) {
-		System.out.println("hao--xi--fu!");
+		System.out.println("hao=-xi=-fu!");
 		System.out.println("Condition!");
 	}
 }
